7b72c1eb24fcd2928cc5602ed31d72ef5d230fcb
quaiats <vinicius.quaiato@gmail.com>
2017-08-19 18:28:58 -0300

    removing hardcoded page limit and removing ifs

--- craw.py
+++ craw.py
@@ -4,6 +4,7 @@ import re, os.path, threading
 from threading import Thread
 
 base_url = "http://magic.wizards.com/en/see-more-wallpaper?page={page}&filter_by=DESC&artist=-1&expansion=&title="
+wanted_wallpaper_resolution_patterns = [r'(?<=download=\")(http\S*2560x1600\S*\.jpg)',r'(?<=download=\")(http\S*1920x1080\S*\.jpg)', r'(?<=download=\")(http\S*1280x960\S*\.jpg)']
 
 def get_url_for_page_index(page_index):
     return base_url.format(page=page_index)
@@ -13,15 +14,15 @@ def load_json_from(url):
     contents = u.read()
     return json.loads(contents.decode())
 
-def load_wallpaper_links(current_url):
-    contents = load_json_from(current_url)['data']
-    wallpaper_links = re.findall(r'(?<=download=\")(http\S*2560x1600\S*\.jpg)', contents, re.MULTILINE)
+def has_data_in(json_data):
+    return json_data != ""
 
-    if(wallpaper_links == []):
-        wallpaper_links = re.findall(r'(?<=download=\")(http\S*1920x1080\S*\.jpg)', contents, re.MULTILINE)
+def load_wallpaper_links(json_data):
+    wallpaper_links = []
 
-    if(wallpaper_links == []):
-        wallpaper_links = re.findall(r'(?<=download=\")(http\S*1280x960\S*\.jpg)', contents, re.MULTILINE)
+    for pattern in wanted_wallpaper_resolution_patterns:
+        if(wallpaper_links == []):
+            wallpaper_links = re.findall(pattern, json_data, re.MULTILINE)
 
     return wallpaper_links
 
@@ -29,20 +30,27 @@ def init_images_downloads_for(wallpaper_links):
     allowed_threads = 5
     for wallpaper in wallpaper_links:
         while(threading.activeCount() > allowed_threads):
-            time.sleep(5)
+            time.sleep(2)
         th = Thread(target=download_file, args=(wallpaper,))
         th.start()
 
 def download_file(wallpaper):
     file_name = "wallpapers/" + wallpaper.split("/")[-1]
     if not os.path.isfile(file_name):
-        print("downloading > " + wallpaper)
+        print("⏬  " + wallpaper)
         urllib.request.urlretrieve(wallpaper, file_name)
 
 def start():
-    for page in range(0, 110):
+    page = 0
+    while True:
         url = get_url_for_page_index(page)
-        wall_paper_urls = load_wallpaper_links(url)
-        init_images_downloads_for(wall_paper_urls)
+        json_data = load_json_from(url)['data']
+        
+        if not has_data_in(json_data):
+            break
+
+        wallpaper_urls = load_wallpaper_links(json_data)
+        init_images_downloads_for(wallpaper_urls)
+        page +=1
 
 start()
